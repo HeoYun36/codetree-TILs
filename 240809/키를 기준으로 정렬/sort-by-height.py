8d07ae35eae30c797acf0cc2700c2db2a1e818e2
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2024-08-09 13:09:50 +0900

    Title: 키를 기준으로 정렬, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240809/키를 기준으로 정렬/sort-by-height.py
@@ -0,0 +1,17 @@
+class info:
+    def __init__(self, name, height, weight):
+        self.name = name
+        self.height = height
+        self.weight = weight
+
+n = int(input())
+
+infos = []
+for _ in range(5):
+    a, b, c = input().split()
+    infos.append(info(a, int(b), int(c)))
+
+infos.sort(key= lambda x: x.height)
+
+for people in infos:
+    print(people.name, people.height, people.weight)
