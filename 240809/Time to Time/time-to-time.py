ec77ca057984a21d60d0666aeb8aaf5a381c66dd
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2024-08-09 19:18:00 +0900

    Title: Time to Time, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240809/Time to Time/time-to-time.py
+++ 240809/Time to Time/time-to-time.py
@@ -1,18 +1,22 @@
-a, b, c, d = map(int, input().split())
+# a, b, c, d = map(int, input().split())
 
-hour, mins = a, b
+# hour, mins = a, b
 
-cnt = 0
+# cnt = 0
 
-while True:
-    if hour == c and mins == d:
-        break
+# while True:
+#     if hour == c and mins == d:
+#         break
 
-    mins += 1 
-    cnt += 1
+#     mins += 1 
+#     cnt += 1
 
-    if mins == 60:
-        hour += 1
-        mins = 0
+#     if mins == 60:
+#         hour += 1
+#         mins = 0
 
-print(cnt)
+# print(cnt)
+
+a, b, c, d = tuple(map(int, input().split()))
+
+print((c * 60 + d) - (a * 60 + b))
