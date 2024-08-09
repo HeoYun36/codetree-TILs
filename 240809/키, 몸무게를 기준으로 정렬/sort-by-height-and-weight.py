e448f799247927b23c932278a6e5560d2c069240
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2024-08-09 15:02:21 +0900

    Title: 키, 몸무게를 기준으로 정렬, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240809/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.py
@@ -0,0 +1,18 @@
+class info:
+    def __init__(self, name, height, weight):
+        self.name = name
+        self.height = height
+        self.weight = weight
+
+n = int(input())
+
+infos = []
+for _ in range(n):
+    a, b, c = input().split()
+    infos.append(info(a, int(b), int(c)))
+
+
+infos.sort(key= lambda x: (x.height, -x.weight))
+
+for people in infos:
+    print(people.name, people.height, people.weight)
