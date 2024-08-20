ff42cef8a414aec1e0188292d4e1248a138f118e
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2024-08-20 21:32:58 +0900

    Title: 두 사람, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240820/두 사람/two-person.py
@@ -0,0 +1,9 @@
+people_list = [input().split() for _ in range(2)]
+
+age = 0
+gender = 1
+
+for i in range(2):
+    if (int(people_list[i][age]) >= 19) and (people_list[i][gender] == 'M'):
+        print(1)
+        break
