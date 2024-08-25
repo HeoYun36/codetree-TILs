7e5957d3b504a2dc1fe181199a43a2ecaa1a4a09
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2024-08-25 17:13:30 +0900

    Title: 369 게임, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240825/369 게임/369-game.py
@@ -0,0 +1,9 @@
+n = input()
+
+for i in range(1, int(n) + 1):
+    if ('3' in str(i)) or ('6' in str(i)) or ('9' in str(i)):
+        print(0, end=' ')
+    elif int(i) % 3 == 0:
+        print(0, end=' ')
+    else:
+        print(i, end=' ')
