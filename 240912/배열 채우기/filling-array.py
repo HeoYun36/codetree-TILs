4c123550c38855afac0159a8445099e08f18c4b6
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2024-09-12 19:32:20 +0900

    Title: 배열 채우기, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240912/배열 채우기/filling-array.py
@@ -0,0 +1,13 @@
+numbers = list(map(int, input().split()))
+
+a = []
+
+for num in numbers:
+    if num == 0:
+        break
+    else:
+        a.append(num)
+
+
+for num in a[::-1]:
+    print(num, end=" ")
