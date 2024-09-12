a178d28532c499dc0e4e2c34bda3b5bd9460a6d1
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2024-09-12 19:27:12 +0900

    Title: 특정 숫자 도달하기, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240912/특정 숫자 도달하기/reaching-specific-number.py
@@ -0,0 +1,16 @@
+numbers1 = list(map(int, input().split()))
+
+numbers2 = []
+for num in numbers1:
+    if num >= 250:
+        break
+    else:
+        numbers2.append(num)
+
+sum_of_numbers2 = 0
+for num in numbers2:
+    sum_of_numbers2 += num
+
+avg = sum_of_numbers2 / len(numbers2)
+
+print(f'{sum_of_numbers2} {avg:.1f}')
