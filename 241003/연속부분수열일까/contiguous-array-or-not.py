05563d94fe8a48103045605f2045192780ec4002
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2024-10-03 15:10:11 +0900

    Title: 연속부분수열일까, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241003/연속부분수열일까/contiguous-array-or-not.py
@@ -0,0 +1,16 @@
+n1, n2 = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+idx_list = []
+result = "Yes"
+
+for i in range(n2):
+    if b[i] in a:
+        idx_list.append(a.index(b[i]))
+
+for i in range(len(idx_list) - 1):
+    if idx_list[i] - idx_list[i + 1] >= 2 or idx_list[i + 1] - idx_list[i] >= 2:
+        result = "No"
+
+print(result)
