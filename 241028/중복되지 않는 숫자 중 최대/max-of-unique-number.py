03e6aabadaa3aa07795a45e83979dacd5c75fc34
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2024-10-28 19:49:53 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

--- 241028/중복되지 않는 숫자 중 최대/max-of-unique-number.py
+++ 241028/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -1,34 +1,20 @@
-import sys
-
 n = int(input())
-n_list = list(map(int, input().split()))
-max_num = -sys.maxsize
-find = False
+nums = list(map(int, input().split()))
 
-while(len(n_list)):
-    cnt = 0
-    # 1. 우선 최댓값을 찾는다.
-    for i in range(len(n_list)):
-        if max_num < n_list[i]:
-            max_num = n_list[i];
+# 최댓값 찾기
+max_num = -1
 
-    # 2. 그 최댓값이 중복되는지 확인
-    for i in range(len(n_list)):
-        if max_num == n_list[i]:
-            cnt += 1
-    
-    # 3. 만약 최댓값이 중복되면 중복되는 위치의 값들을 초기화
-    if cnt > 1:
-        for i in range(len(n_list)):
-            if max_num == n_list[i]:
-                n_list[i] = -sys.maxsize
-        # 최대값 초기화
-        max_num = -sys.maxsize
+for i in range(n):
+    #최대가 될 수 있는 후보
+    if max_num < nums[i]:
+        # 각각의 숫자를 조사함
         cnt = 0
-    else:
-        break
+        for j in range(n):
+            if nums[j] == nums[i]:
+                cnt += 1
 
-if max_num == -sys.maxsize:
-    print(-1)
-else:
-    print(max_num)
+        # 이 숫자가 배열에서 유일할때만 갱신
+        if cnt == 1:
+            max_num = nums[i]
+
+print(max_num)
