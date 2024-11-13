4a006807f82813c0aa0fd640ea254fdf23f9c17a
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2024-11-13 16:17:57 +0900

    Title: 배열의 평균, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241113/배열의 평균/ave-of-array.py
@@ -0,0 +1,27 @@
+# 반복문으로 2번 반복해서 4열을 입력받아 2행 4열을 입력받음
+arr = [list(map(int, input().split()) )for _ in range(2)]
+
+# 행 평균(2번)
+for i in range(2):
+    sum_of_row = 0
+    for j in range(4):
+        sum_of_row += arr[i][j]
+    print(sum_of_row / 4, end=' ')
+
+print()
+
+# 열 평균(4번)
+for j in range(4):
+    sum_of_col = 0
+    for i in range(2):
+        sum_of_col += arr[i][j]
+    print(sum_of_col / 2, end=' ')
+
+print()
+
+# 전체 평균
+sum_of_all = 0
+for i in range(2):
+    for j in range(4):
+        sum_of_all += arr[i][j]
+print(sum_of_all / 8)
