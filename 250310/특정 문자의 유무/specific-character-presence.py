0fff83606a99308583453f95a0f96ca978f42a6b
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2025-03-10 14:18:06 +0900

    Title: 특정 문자의 유무, Time: 50ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250310/특정 문자의 유무/specific-character-presence.py
@@ -0,0 +1,11 @@
+str_input = input()
+
+if "ee" in str_input:
+    print("Yes", end=' ')
+else:
+    print("No", end=' ')
+
+if "ab" in str_input:
+    print("Yes", end=' ')
+else:
+    print("No", end=' ')    
