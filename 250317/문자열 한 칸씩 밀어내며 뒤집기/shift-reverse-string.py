2a248e0801e8ae92d67465f2706cd40ea49192d3
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2025-03-17 14:16:30 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 78ms, Memory: 18MB, Status: Passed - CodeTree

--- 250317/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.py
+++ 250317/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.py
@@ -3,4 +3,14 @@ q = int(q)
 queries = [int(input()) for _ in range(q)]
 
 # Please write your code here
-print(queries)
+for i in range(q):
+    if queries[i] == 1:
+        input_str = input_str[1:] + input_str[0]
+        print(input_str)
+    elif queries[i] == 2:
+        input_str = input_str[-1] + input_str[:len(input_str) - 1]
+        print(input_str)
+    elif queries[i] == 3:
+        input_str = input_str[-1::-1]
+        print(input_str)
+        
