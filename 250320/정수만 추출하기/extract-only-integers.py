7183111bb91abbd1b09c6d2e898a77dc964efe0e
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2025-03-20 11:04:09 +0900

    Title: 정수만 추출하기, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250320/정수만 추출하기/extract-only-integers.py
+++ 250320/정수만 추출하기/extract-only-integers.py
@@ -5,7 +5,7 @@ sum_of_num = 0
 def str_f(input_str):
     result = ''
     for i in range(len(input_str)):
-        if '0' <= input_str[i] and input_str <= '9':
+        if '0' <= input_str[i] and input_str[i] <= '9':
             result += input_str[i]
         else:
             break
