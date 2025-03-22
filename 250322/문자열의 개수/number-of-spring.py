8bf4d15f6b7927deea7cc86b1071a54a1216b9ba
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2025-03-22 11:37:18 +0900

    Title: 문자열의 개수, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250322/문자열의 개수/number-of-spring.py
@@ -0,0 +1,14 @@
+arr = []
+
+while True:
+    str_input = input()
+    arr.append(str_input)
+
+    if str_input == '0':
+        break
+
+print(len(arr) - 1)
+
+for i in range(0, len(arr), 2):
+    print(arr[i])
+
