511b009561adcf01df4c87b1d82b64695708a91a
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2025-03-29 21:28:26 +0900

    Title: 함수를 이용한 369 게임, Time: 127ms, Memory: 18MB, Status: Passed - Codetree

--- 250329/함수를 이용한 369 게임/369-games-using-functions.py
+++ 250329/함수를 이용한 369 게임/369-games-using-functions.py
@@ -4,24 +4,20 @@ a, b = map(int, input().split())
 def f_a(num):
     arr = list(str(num))
     for i in range(len(arr)):
-        if arr[i] == 3 or arr[i] == 6 or arr[i] == 9:
-            return True
+        if arr[i] == '3' or arr[i] == '6' or arr[i] == '9':
+            return True   
 
-f_a(23)
-    
+def f_b(num):
+    return num % 3 == 0
 
-# def f_b(num):
-#     return num % 3 == 0
-
-# def is_magic_number(n):
-#     return f_a(n) or f_b(n)
+def is_magic_number(n):
+    return f_a(n) or f_b(n)
         
-# cnt = 0
-# for i in range(a, b + 1):
-#     if is_magic_number(i):
-#         print(i)
-#         cnt += 1
+cnt = 0
+for i in range(a, b + 1):
+    if is_magic_number(i):
+        cnt += 1
 
-# print(cnt)
+print(cnt)
 
 
