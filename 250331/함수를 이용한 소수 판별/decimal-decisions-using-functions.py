744e33781cd9fdeb790c51a0cb16aaa753f238de
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2025-03-31 12:18:34 +0900

    Title: 함수를 이용한 소수 판별, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250331/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,16 @@
+a, b = map(int, input().split())
+
+# Please write your code here.
+def is_prime(n):
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+
+    return True
+
+sum_of_num = 0
+for i in range(a, b + 1):
+    if is_prime(i):
+        sum_of_num += i
+
+print(sum_of_num)
