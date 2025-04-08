2ef2118298d7be4ed10a63dc930731f566807a63
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2025-04-08 11:34:33 +0900

    Title: 2021년 날짜의 유무, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250408/2021년 날짜의 유무/with-or-without-2021.py
+++ 250408/2021년 날짜의 유무/with-or-without-2021.py
@@ -1,20 +1,27 @@
 M, D = map(int, input().split())
 
 # Please write your code here.
-
+    
 def magic_f(m, d):
     if m > 12:
         return "No"
     elif m == 2:
-        if d < 1 or d > 28:
+        if d > 28:
             return "No"
-    elif m % 2 == 0:
-        if d < 1 or d > 30:
+    elif m >= 1 and m <= 7:
+        if m % 2 == 0:
+           if d > 30:
             return "No"
-    elif m % 2 != 0:
-        if d < 1 or d > 31:
-            return "No"
-    
+        else:
+            if d > 31:
+                return "No"
+    else:
+        if m % 2 == 0:
+            if d > 31:
+                return "No"
+        else:
+            if d > 30:
+                return "No"    
     return "Yes"
 
 print(magic_f(M, D))
