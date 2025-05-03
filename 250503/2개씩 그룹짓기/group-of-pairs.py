182d9ae9c1f33ef6e7c59eb2c76076e9926b6df3
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2025-05-03 16:59:11 +0900

    Title: 2개씩 그룹짓기, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250503/2개씩 그룹짓기/group-of-pairs.py
@@ -0,0 +1,14 @@
+n = int(input())
+nums = list(map(int, input().split()))
+
+# Please write your code here.
+nums.sort()
+arr = []
+
+for i in range(n):
+    arr.append(nums[i] + nums[2 * n - (i + 1)])
+
+arr.sort(reverse=True)
+
+print(arr[0])
+
