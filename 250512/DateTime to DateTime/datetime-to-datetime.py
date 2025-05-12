efec14065008121d4eedc445be883ba84dd4e242
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2025-05-12 12:40:48 +0900

    Title: DateTime to DateTime, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250512/DateTime to DateTime/datetime-to-datetime.py
+++ 250512/DateTime to DateTime/datetime-to-datetime.py
@@ -1,3 +1,9 @@
 a, b, c = map(int ,input().split())
 
-print((24 * 60 * a + 60 * b + c) - (24 * 60 * 11 + 60 * 11 + 11))
+
+result = (24 * 60 * a + 60 * b + c) - (24 * 60 * 11 + 60 * 11 + 11)
+
+if result < 0:
+    print(-1)
+else:
+    print(result)
