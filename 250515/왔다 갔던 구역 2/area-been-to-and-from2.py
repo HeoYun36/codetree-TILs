c0ef1b9f0d92555d69a21d59a9c09d8bd893a1d4
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2025-05-15 12:52:29 +0900

    Title: 왔다 갔던 구역 2, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250515/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,30 @@
+n = int(input())
+
+slot = [0] * 4001
+curr_state = 1999
+
+for i in range(n):
+    x, d = input().split()
+    x = int(x)
+    if d == "R":
+        while x:
+            slot[curr_state] += 1
+            curr_state += 1
+
+            x -= 1
+    else:
+        while x:
+            slot[curr_state] += 1
+            curr_state -= 1
+
+            x -= 1
+
+length = 0
+
+for i in range(len(slot)):
+    if slot[i] >= 2:
+        length +=1
+
+print(length)
+
+
