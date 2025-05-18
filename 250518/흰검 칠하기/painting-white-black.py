093679fc97f4f0914a781b9222e78aa88c492c74
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2025-05-18 22:02:50 +0900

    Title: 흰검 칠하기, Time: 158ms, Memory: 39MB, Status: Passed - Codetree

--- 250518/흰검 칠하기/painting-white-black.py
+++ 250518/흰검 칠하기/painting-white-black.py
@@ -1,6 +1,7 @@
 n = int(input())
 
-slot = [[0, 0, 0] for _ in range(199999)]
+# [현재 색깔, 흰 cnt, 검정 cnt]
+slot = [['', 0, 0] for _ in range(199999)]
 
 state = 99999
 
@@ -8,25 +9,26 @@ for _ in range(n):
     x, d = input().split()
     x = int(x)
 
-    if d == 'R':
-        for i in range(state, state + x):
-            if slot[i][0] != 'g':
-                slot[i][1] += 1
-                if slot[i][1] == 2 and slot[i][2] == 2:
-                    slot[i][0] = 'g'
-                else:
-                    slot[i][0] = "b"
-        state += (x - 1)
-    else:
+    # 왼쪽
+    if d == 'L':
         for i in range(state - x + 1, state + 1):
             if slot[i][0] != 'g':
-                slot[i][2] += 1
-                if slot[i][1] == 2 and slot[i][2] == 2:
+                slot[i][1] += 1
+                slot[i][0] = "w"
+                if slot[i][1] >= 2 and slot[i][2] >= 2:
                     slot[i][0] = 'g'
-                else:
-                    slot[i][0] = "w"
-
+                    
         state -= (x - 1)
+    # 오른쪽
+    else:
+        for i in range(state, state + x):
+            if slot[i][0] != 'g':
+                slot[i][2] += 1
+                slot[i][0] = "b"
+                if slot[i][1] >= 2 and slot[i][2] >= 2:
+                    slot[i][0] = 'g'
+
+        state += (x - 1)
 
 white = 0
 black = 0
