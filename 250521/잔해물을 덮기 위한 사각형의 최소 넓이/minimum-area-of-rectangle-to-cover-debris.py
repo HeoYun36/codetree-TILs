dd2be4b994c2e54ca22637d2543cc778a0e13b65
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2025-05-21 16:09:24 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 140ms, Memory: 48MB, Status: Passed - Codetree

--- 250521/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 250521/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -1,5 +1,4 @@
 a_x1, a_y1, a_x2, a_y2 = map(int ,input().split())
-
 b_x1, b_y1, b_x2, b_y2 = map(int ,input().split())
 
 offset = 1000
@@ -24,29 +23,35 @@ for i in range(b_x1, b_x2):
     for j in range(b_y1, b_y2):
         arr_2d[i][j] -= 1
 
-x_arr = []
-y_arr = []
+
+max_x2 = 0
+max_y2 = 0
+
+min_x1 = 2000
+min_y1 = 2000
 
 for i in range(2001):
     for j in range(2001):
         if arr_2d[i][j] == 1:
-            x_arr.append(i)
-            y_arr.append(j)
+            if max_x2 <= i:
+                max_x2 = i
 
-if x_arr == [] and y_arr == []:
-    print(0)
-else:
-    max_x2 = max(x_arr) + 1
-    max_y2 = max(y_arr) + 1
+            if max_y2 <= j:
+                max_y2 = j
 
-    min_x1 = min(x_arr)
-    min_y1 = min(y_arr)
+            if min_x1 >= i:
+                min_x1 = i
+            
+            if min_y1 >= j:
+                min_y1 = j
 
-    area = 0
+area = 0
+for i in range(min_x1, max_x2 + 1):
+    for j in range(min_y1, max_y2 + 1):
+        area +=1
 
-    for i in range(min_x1, max_x2):
-        for j in range(min_y1, max_y2):
-            area += 1
+print(area)
 
-    print(area)
+
+
 
