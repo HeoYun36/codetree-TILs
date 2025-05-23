c2b3294e2d39ba23f4ee7a7fea22c166905de64d
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2025-05-23 17:48:13 +0900

    Title: 연속되는 수 2, Time: 66ms, Memory: 16MB, Status: Passed - Codetree

--- 250523/연속되는 수 2/continuous-number2.py
+++ 250523/연속되는 수 2/continuous-number2.py
@@ -6,18 +6,23 @@ for _ in range(n):
     num = int(input())
     num_list.append(num)
 
-cnt = 1
+if n == 1:
+    print(1)
+else:
+    cnt = 1
 
-arr = []
+    arr = []
 
-for i in range(1, n):
-    if num_list[i] != num_list[i - 1]:
-        arr.append(cnt)
-        cnt = 1
-    else:
-        cnt += 1
 
-    if i == n - 1:
-        arr.append(cnt)
 
-print(max(arr))
+    for i in range(1, n):
+        if num_list[i] != num_list[i - 1]:
+            arr.append(cnt)
+            cnt = 1
+        else:
+            cnt += 1
+
+        if i == n - 1:
+            arr.append(cnt)
+
+    print(max(arr))
