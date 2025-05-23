8d2c76e7074a749dd6f2df0d25504db185dbf8b8
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2025-05-23 20:51:14 +0900

    Title: 연속되는 수 3, Time: 82ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250523/연속되는 수 3/continuous-number3.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+num_list = []
+
+for _ in range(n):
+    num = int(input())
+    num_list.append(num)
+
+cnt = 1
+
+cnt_list = []
+
+for i in range(1, n):
+    if num_list[i - 1] * num_list[i] < 0:
+        cnt_list.append(cnt)
+        cnt = 0
+
+    cnt += 1
+
+    if i == n - 1:
+        cnt_list.append(cnt)
+
+print(max(cnt_list))
