9f50146b6c473a441b67e5588b5c8ef0cb8c92ff
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2025-05-23 20:52:14 +0900

    Title: 연속되는 수 3, Time: 53ms, Memory: 16MB, Status: Passed - Codetree

--- 250523/연속되는 수 3/continuous-number3.py
+++ 250523/연속되는 수 3/continuous-number3.py
@@ -6,18 +6,21 @@ for _ in range(n):
     num = int(input())
     num_list.append(num)
 
-cnt = 1
+if n == 1:
+    print(1)
+else:
+    cnt = 1
 
-cnt_list = []
+    cnt_list = []
 
-for i in range(1, n):
-    if num_list[i - 1] * num_list[i] < 0:
-        cnt_list.append(cnt)
-        cnt = 0
+    for i in range(1, n):
+        if num_list[i - 1] * num_list[i] < 0:
+            cnt_list.append(cnt)
+            cnt = 0
 
-    cnt += 1
+        cnt += 1
 
-    if i == n - 1:
-        cnt_list.append(cnt)
+        if i == n - 1:
+            cnt_list.append(cnt)
 
-print(max(cnt_list))
+    print(max(cnt_list))
