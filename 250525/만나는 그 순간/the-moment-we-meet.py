f7d065b7bf321ac8f6199680a5842f3a0e1a53be
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2025-05-25 15:58:16 +0900

    Title: 만나는 그 순간, Time: 92ms, Memory: 28MB, Status: Passed - Codetree

--- 250525/만나는 그 순간/the-moment-we-meet.py
+++ 250525/만나는 그 순간/the-moment-we-meet.py
@@ -1,10 +1,9 @@
 n, m = map(int, input().split())
 
-a = [0 for _ in range(1000001)]
-b = [0 for _ in range(1000001)]
+a = [0]
+b = [0]
 
 # A
-idx = 0
 state = 0
 
 for _ in range(n):
@@ -21,9 +20,8 @@ for _ in range(n):
 
     # 주어진 시간만큼 이동하면서 리스트에 위치 기록
     while t != 0:
-        idx += 1
         state += move
-        a[idx] = state
+        a.append(state)
 
         t -= 1
 
@@ -47,18 +45,17 @@ for _ in range(m):
     while t != 0:
         idx += 1
         state += move
-        b[idx] = state
+        b.append(state)
 
         t -= 1
 
 sec = -1
 # 몇 초 뒤에 만나는지 확인
-for i in range(1, 1000001):
+length = max(len(a), len(b))
+
+for i in range(1, length):
     if a[i] == b[i]:
         sec = i
         break
 
 print(sec)
-
-
-
