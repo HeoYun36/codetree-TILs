34ab95a592f4cd551a9c233ee20dbd92c8b98620
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2025-05-25 16:24:33 +0900

    Title: 만나는 그 순간, Time: 99ms, Memory: 27MB, Status: Passed - Codetree

--- 250525/만나는 그 순간/the-moment-we-meet.py
+++ 250525/만나는 그 순간/the-moment-we-meet.py
@@ -3,6 +3,24 @@ n, m = map(int, input().split())
 a = [0]
 b = [0]
 
+def move(arr, d, t, state):
+     # 방향 설정
+    move_dir = 0
+
+    if d == 'R':
+        move_dir = 1
+    else:
+        move_dir = -1
+
+    # 주어진 시간만큼 이동하면서 리스트에 위치 기록
+    while t != 0:
+        state += move_dir
+        arr.append(state)
+
+        t -= 1
+    
+    return state
+
 # A
 state = 0
 
@@ -10,20 +28,7 @@ for _ in range(n):
     d, t = input().split()
     t = int(t)
 
-    # 방향 설정
-    move = 0
-
-    if d == 'R':
-        move = 1
-    else:
-        move = -1
-
-    # 주어진 시간만큼 이동하면서 리스트에 위치 기록
-    while t != 0:
-        state += move
-        a.append(state)
-
-        t -= 1
+    state = move(a, d, t, state)
 
 # B
 state = 0
@@ -32,23 +37,9 @@ for _ in range(m):
     d, t = input().split()
     t = int(t)
 
-    # 방향 설정
-    move = 0
-
-    if d == 'R':
-        move = 1
-    else:
-        move = -1
-
-    # 주어진 시간만큼 이동하면서 리스트에 위치 기록
-    while t != 0:
-        state += move
-        b.append(state)
-
-        t -= 1
+    state = move(b, d, t, state)
 
 sec = -1
-# 몇 초 뒤에 만나는지 확인
 length = max(len(a), len(b))
 
 for i in range(1, length):
