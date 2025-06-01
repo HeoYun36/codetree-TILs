d6ab7c49bce8b6fc04762da03797663d353209d7
Yun Heo <115468226+HeoYun36@users.noreply.github.com>
2025-06-01 16:54:09 +0900

    Title: 작은 구슬의 이동, Time: 78ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250601/작은 구슬의 이동/small-marble-movement.py
@@ -0,0 +1,30 @@
+n, t = map(int, input().split())
+
+r, c, d = input().split()
+r, c = int(r), int(c)
+
+dr = [0, 1, -1, 0]
+dc = [1, 0, 0, -1]
+
+def f(x, y):
+    return 0 < x and x <= n and 0 < y and y <= n
+
+dir_dic = {
+    "R" : 0,
+    "N" : 1,
+    "S" : 2,
+    "L" : 3
+}
+
+dir_num = dir_dic[d]
+for _ in range(t):
+    nr = r + dr[dir_num]
+    nc = c + dc[dir_num]
+
+    if not f(nr, nc):
+        dir_num = 3 - dir_num
+    else:
+        r = r + dr[dir_num]
+        c = c + dc[dir_num]        
+
+print(r, c)
